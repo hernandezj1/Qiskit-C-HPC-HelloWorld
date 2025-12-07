45c9d919467dd4ef6d83bfe3347bb66243f93b99
Jose Hernandez <98195541+hernandezj1@users.noreply.github.com>
2025-12-07 13:42:16 -0600

    Create Build.sh

---
+++ Build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script builds all 4 sample files in qiskit-CPP
+cd qiskit-cpp/samples
+mkdir build
+cd build 
+cmake  -DQISKIT_ROOT=../../../qiskit/ -DCMAKE_EXE_LINKER_FLAGS="-Wl,-rpath=$CONDA_PREFIX/lib" -DQRMI_ROOT=/../../../qrmi/ ..
+make
+
+# if you want to build with qiskit runtime instead run the following:
+#cmake  -DQISKIT_ROOT=../../../qiskit/ -DCMAKE_EXE_LINKER_FLAGS="-Wl,-rpath=$CONDA_PREFIX/lib" -DQISKIT_IBM_RUNTIME_C_ROOT=../../../qiskit-ibm-runtime-c/ ..
+#make
+
