e08972575523c609c84090c236d60c250584559c
Jose Hernandez <98195541+hernandezj1@users.noreply.github.com>
2025-12-07 14:15:48 -0600

    Create Submit_script.sh

---
+++ Submit_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -J qiskit-job
+#SBATCH -t 00:30:00
+#SBATCH -A genacc_q
+#SBATCH -n 1
+
+#This will vary by HPC center but make sure you can access the web for submission
+module load webproxy
+
+# Load conda and activate your environment
+module load anaconda
+source activate ~/.conda/envs/quantum_c
+
+./qiskit-cpp/samples/build/sampler_test
